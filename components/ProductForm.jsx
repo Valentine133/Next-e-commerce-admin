60627c5a42f6a6e1a1d0ba1a060da1f826280746
Valentine <v.zhyvotchenko@gmail.com>
2023-07-28 10:26:26 +0200

    Add image input for upload file

--- components/ProductForm.jsx
+++ components/ProductForm.jsx
@@ -7,7 +7,8 @@ const ProductForm = ({
   title: existingTitle, 
   sku: existingSku, 
   description: existingDescription, 
-  price: existingPrice
+  price: existingPrice,
+  images,
 }) => {
   const [title, setTitle] = useState(existingTitle || '');
   const [description, setDescription] = useState(existingDescription  || '');
@@ -34,8 +35,38 @@ const ProductForm = ({
     router.push('/products');
   }
 
+  const uploadImage = async (e) => {
+    const files = e.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append('file', file);
+      }
+      await fetch('/api/upload', {
+        method: 'POST',
+        body: data,
+      });
+    }
+  }
+
   return (
     <form onSubmit={saveProduct}>
+      <label>Images</label>
+      <div className='mb-3'>
+        <div className="flex items-center justify-center w-full">
+            <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                    <p className="text-xs text-gray-500 dark:text-gray-400">PNG, JPG (MAX. 2MB)</p>
+                </div>
+                <input onChange={uploadImage} id="dropzone-file" type="file" className="hidden" />
+            </label>
+        </div>
+        {!images?.length && (
+          <div>No images in this product</div>
+        )}
+      </div>
       <label>Title</label>
       <input 
         className='form-control' 
